8b1df27ed31385c411dccd39814f497472a6ee62
stbugyei <stbugyei@yahoo.co.uk>
2020-03-27 10:31:36 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -17,7 +17,8 @@ function App() {
         <div className='wrapper'>
           <Router>
             <Navbar /> 
-            <Route path="/" component={WhyWeDo}></Route>
+            <Route exact path="/" component={WhyWeDo}></Route>
+            <Route path="/whyWeDo" component={WhyWeDo}></Route>
             <Route path="/whatWeDo" component={WhatWeDo}></Route>
             <Route path="/careers" component={Careers}></Route>
             <Route path="/press" component={Press}></Route>
